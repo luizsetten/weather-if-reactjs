0e81b16fd5aff69e946109abae0ad9b3bf5ba98b
Luiz Gustavo Chinelato Setten <luizgustavosetten@hotmail.com>
2022-05-23 01:16:13 -0300

    feat: Logout when user doesn't have acess

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,4 +1,5 @@
 import Axios from "axios";
+import { toast } from "react-toastify";
 
 const api = Axios.create({
   baseURL: "http://192.168.0.112:3333/",
@@ -8,6 +9,20 @@ api.interceptors.request.use((config) => {
   const token = sessionStorage.getItem("@weatherData/userToken");
   // eslint-disable-next-line no-param-reassign
   if (token) config.headers = { authorization: `Bearer ${token}` };
+  return config;
+});
+
+api.interceptors.response.use((config) => {
+  if (config.status === 401)
+    sessionStorage.removeItem("@weatherData/userToken");
+  // Navegar para tela inicial
+
+  if (config.status >= 300 && (config.data.message || config.data.error)) {
+    return config.data.message
+      ? toast.error(config.data.message)
+      : toast.error(config.data.error);
+  }
+
   return config;
 });
 
